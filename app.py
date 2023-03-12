730bef2b41b334605539c8e82639a10103ca04f9
Angel <angelaltarodri@gmail.com>
2023-03-12 11:46:07 -0500

    leyton

--- app.py
+++ app.py
@@ -7,12 +7,17 @@ import growattServer
 api = growattServer.GrowattApi(False, "my-user-id")
 login_response = api.login(username, user_pass)
 plant_list = api.plant_list(login_response['user']['id'])
+plant_info_LEYTON = api.plant_info(1626659)
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return plant_list
+
+@app.route("/leyton")
+def leyton():
+    return plant_info_LEYTON
 
 if __name__ == '__main__':
     app.run(debug=True, port=8001)
