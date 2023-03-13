fb757d53d43aecab58e8598779163988a4c45317
Angel <angelaltarodri@gmail.com>
2023-03-13 14:48:38 -0500

    now with id on URL

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ user_pass = '123456'
 api = growattServer.GrowattApi(False, "my-user-id")
 login_response = api.login(username, user_pass)
 plant_list = api.plant_list(login_response['user']['id'])
-plant_info_LEYTON = api.plant_info(1626659)
 
 app = Flask(__name__)
 CORS(app)
@@ -18,9 +17,10 @@ CORS(app)
 def home():
     return plant_list
 
-@app.route("/leyton")
-def leyton():
-    return plant_info_LEYTON
+@app.route("/<string:plantId>")
+def leyton(plantId):
+    plant_info = api.plant_info(plantId)
+    return plant_info
 
 if __name__ == '__main__':
     app.run(debug=True, port=8001)
