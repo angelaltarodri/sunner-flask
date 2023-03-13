2c58c2a99473d996baab3c8598862da7cbc3c962
Angel <angelaltarodri@gmail.com>
2023-03-13 13:59:55 -0500

    new requirement

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
 
+import growattServer
+
 username = 'sunnerperu'
 user_pass = '123456'
 
-import growattServer
 api = growattServer.GrowattApi(False, "my-user-id")
 login_response = api.login(username, user_pass)
 plant_list = api.plant_list(login_response['user']['id'])
@@ -13,7 +14,7 @@ plant_info_LEYTON = api.plant_info(1626659)
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/", methods=['GET'])
+@app.route("/")
 def home():
     return plant_list
 
