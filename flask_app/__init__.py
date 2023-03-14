62367eae171fca9c101886e9ce9969cb44b4a580
Angel <angelaltarodri@gmail.com>
2023-03-14 17:17:11 -0500

    if !NUMBER return NO

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask import jsonify
+
 from flask_cors import CORS
 import sys
 import logging
 
 import growattServer
+
+app = Flask(__name__)
 
 username = 'sunnerperu'
 user_pass = '123456'
@@ -11,26 +15,29 @@ user_pass = '123456'
 api = growattServer.GrowattApi(False, "my-user-id")
 login_response = api.login(username, user_pass)
 plant_list = api.plant_list(login_response['user']['id'])
-
-app = Flask(__name__)
 CORS(app)
+
 
 @app.route("/")
 def home():
-    return plant_list
+    return jsonify(plant_list)
+
 
 @app.route("/<string:plantId>")
 def leyton(plantId):
+    if not plantId.isnumeric():
+        return jsonify(message="Plant ID inválido")
     plant_info = api.plant_info(plantId)
-    return plant_info
+    return jsonify(plant_info)
 
-@app.route("/cortez")
+
+@app.route("/gabo")
 def gabo():
     plant_info = api.plant_info(1581547)
-    return plant_info
+    return jsonify(plant_info)
+
 
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
 
-if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+app.secret_key = "Mi clave super mega secreta"
